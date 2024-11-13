edeca92a123f05fa8306f64f9bd14a8e20748207
Nusrat Trix <83204591+nusrattrix@users.noreply.github.com>
2024-11-13 21:12:55 +0600

    index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
 });
 
 const statusMessages = ["🎧 Listening to Spotify", "🍩 Assisting Trix", "🎮 Playing Genshin Impact✨"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusTypes = [ 'idle', 'dnd', 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
